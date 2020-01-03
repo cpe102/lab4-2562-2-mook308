7cbcd268ee2496ebb1f41baf7d3bf2669d5cb599
mook <keettamook_k@cmu.ac.th>
2020-01-03 15:23:11 +0700

    finish 4_3

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
 
+int findDivisor(int x){
+  int i = 2;
+  while (i>1){
+      if(x%i==0){
+          return i;
+      }else{
+          i++;
+      }
+  }
+
+}
+
+int main(){
+    int x;
+
+    cout << "Enter number = ";
+    cin >> x;
+    cout << "\n";
+    cout << "Divisor is " << findDivisor(x);
+
+  
+
+    return 0;
+}
