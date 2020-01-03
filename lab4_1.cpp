9bc1425d275edbe64433b44b1c190798ba303c8f
mook <keettamook_k@cmu.ac.th>
2020-01-03 14:55:43 +0700

    finish 4_1

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,30 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
 
+
+    float ans(float x){
+
+        float y = (3*pow(x,3)) + (2*2.71828) + pow(2,((2*x)+1))+ sqrt((pow(x,2))+1);
+
+        return y;
+        }
+
+
+int main()
+{
+    float x;
+    cout << "Enter x = ";
+    cin >> x;
+    cout << "\n";
+    cout << "Result y = ";
+
+
+    cout <<ans(x);
+
+    return 0;
+
+
+
+
+}
